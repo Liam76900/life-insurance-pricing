ef94efd3bdcac7fa2e6ed1eb2525933af83b240b
Liam76900 <ljthomson@gmail.com>
2026-08-12 13:03:31 +0100

    Added function to generate mortality table

---
+++ generate_table.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+
+ages = np.arange(0, 111)
+A = 0.0001
+B = 0.00005
+c = 1.1
+
+qx = A + B * (c ** ages)
+qx = np.clip(qx, 0, 1)
+
+mortality_table = pd.DataFrame({'age': ages, 'qx': qx})
+mortality_table.to_csv('mortality_table.csv', index=False)
